609924be03c26d3ba46bcc6832275aab630fd85f
antkneevee <anthonyventriello@gmail.com>
2025-06-20 15:59:48 -0400

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
 </Link>
       <Link
         to="/editor"
-        className={\`flex items-center mb-6 text-gray-700 hover:text-orange-500 \${isActive('/editor') ? 'font-bold text-orange-600' : ''}\`}
+        className={`flex items-center mb-6 text-gray-700 hover:text-orange-500 ${isActive('/editor') ? 'font-bold text-orange-600' : ''}\`}
       >
         <Edit3 className="w-6 h-6 mr-0 lg:mr-2" />
         <span className="hidden lg:inline">Editor</span>
